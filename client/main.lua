ae3376d116bcb1b6b4297e7863141fa4b48e3a14
MateHUNAa <you@example.com>
2025-11-07 00:47:35 +0100

    feat(loaditems): added register garden items
    Automatically registering oxInv items with mGardening tag. also waiting for the client to fully
    load.

--- client/main.lua
+++ client/main.lua
@@ -2,7 +2,29 @@
 
 local pos = vec4(53.0448, -1894.7394, 21.6000-0.9, 52.7274)
 
+function LoadGardenItems()
+     for item, data in pairs(inv:Items()) do
+          if data and data.client then
+               local cData = data.client
+
+               if cData.mGardening then
+
+                    --- @type Plant
+                    local plantData = cData.plantData
+
+                    PlantRegistry:Register(plantData.name, plantData)
+                    Logger:Info("Registered plant: " .. plantData.name, plantData)
+               end
+          end
+     end
+end
+
 Citizen.CreateThread(function (threadId)
+     while not ESX.IsPlayerLoaded() do
+          Wait(250)
+     end
+     LoadGardenItems()
+
 	local garden = Garden:new(pos.xyz, 5,5)
 
      while true do
