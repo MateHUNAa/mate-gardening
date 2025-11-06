12eb9e1c3ae75ed30975455d5f0c85b5ed90b946
MateHUNAa <you@example.com>
2025-11-07 00:46:44 +0100

    style: add Type safety
    Added classes for parameteres for type safety

--- client/garden/PlantRegistry.lua
+++ client/garden/PlantRegistry.lua
@@ -2,13 +2,13 @@ PlantRegistry = {}
 PlantRegistry.__index = PlantRegistry
 
 --- @param id string
---- @param data table
+--- @param data Plant
 function PlantRegistry:Register(id, data)
      self[id] = data
 end
 
 --- @param id string
---- @return table|boolean
+--- @return Plant|boolean
 function PlantRegistry:Get(id)
 	return self[id] or false
 end
