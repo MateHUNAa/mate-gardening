f9dea336cd8ba7404dba6bd067a1f23e3e1ac358
MateHUNAa <you@example.com>
2025-11-07 00:24:29 +0100

    chore: project generated with template: mCore
    Project generated with template mCore(reactTsx, mate-grid)

---
+++ shared/config.lua
@@ -0,0 +1,24 @@
+Config = {
+     lan               = "en",
+     PedRenderDistance = 80.0,
+     target            = true,
+     eventPrefix       = "mhScripts"
+}
+
+Config.MHAdminSystem = GetResourceState("mate-admin") == "started"
+
+Config.ApprovedLicenses = {
+     "license:123",
+     "fivem:123",
+     "discord:123",
+     "live:123",
+     "steam:123",
+     "xbl:123"
+}
+
+Config.Grid = {
+     cellSize = 0.8
+}
+
+
+Loc = {}
