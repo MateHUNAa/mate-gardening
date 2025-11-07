d7673596d16494d86d4129315a147c2fb35a93a5
MateHUNAa <you@example.com>
2025-11-07 01:29:39 +0100

    docs: change doc-name from Plant-> PlantData

--- client/garden/PlantRegistry.lua
+++ client/garden/PlantRegistry.lua
@@ -2,13 +2,13 @@ PlantRegistry = {}
 PlantRegistry.__index = PlantRegistry
 
 --- @param id string
---- @param data Plant
+--- @param data PlantData
 function PlantRegistry:Register(id, data)
      self[id] = data
 end
 
 --- @param id string
---- @return Plant|boolean
+--- @return PlantData|boolean
 function PlantRegistry:Get(id)
 	return self[id] or false
 end
