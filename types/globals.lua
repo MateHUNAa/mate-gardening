90a538a21966014d0ab0dd40314bd0053db84477
MateHUNAa <you@example.com>
2025-11-13 19:30:57 +0100

    docs: added DrawCustomIcon doc

--- types/globals.lua
+++ types/globals.lua
@@ -28,3 +28,16 @@ function Success(msg) end
 ---@param callback fun(playerId:number, identifier: string, params: table): any
 ---@param showLog? boolean
 function regServerNuiCallback(eventName, callback, showLog) end
+
+
+mCore = {}
+
+---@param center vector3
+---@param width number
+---@param height number
+---@param icon string|table
+---@param rot vector3|nil
+---@param dir vector3|nil
+---@param color table|nil
+---@param faceCamera boolean
+mCore.DrawCustomIcon = function(center, width, height, icon, rot, dir, color, faceCamera) end
