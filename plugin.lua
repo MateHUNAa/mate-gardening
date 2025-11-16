c3b16f425a3e72108e89f026d274748acac9ead6
MateHUNAa <you@example.com>
2025-11-16 16:11:17 +0100

    ci(luarc): plugin update
    idk tbh

--- plugin.lua
+++ plugin.lua
@@ -2,9 +2,14 @@ local str_find = string.find
 local str_sub = string.sub
 local str_gmatch = string.gmatch
 
----@param uri string # The uri of file
----@param text string # The content of file
----@return { start: integer, finish: integer, text: string }[] | string | nil
+---@class diff
+---@field start  integer # The number of bytes at the beginning of the replacement
+---@field finish integer # The number of bytes at the end of the replacement
+---@field text   string  # What to replace
+
+---@param  uri  string # The uri of file
+---@param  text string # The content of file
+---@return nil|diff[]
 function OnSetText(uri, text)
      -- ignore .vscode dir, extension files (i.e. natives), and other meta files
      if str_find(uri, '[\\/]%.vscode[\\/]') or str_sub(text, 1, 8) == '---@meta' then return end
@@ -14,6 +19,14 @@ function OnSetText(uri, text)
 
      local diffs = {}
      local count = 0
+
+     for start, realName, finish in text:gmatch [=[require[ ]*["']()__(.-)()["']]=] do
+             diffs[#diffs+1] = {
+                 start  = start,
+                 finish = finish - 1,
+                 text   = realName,
+             }
+     end
 
      -- prevent diagnostic errors in fxmanifest.lua and __resource.lua files
      if str_find(uri, 'fxmanifest%.lua$') or str_find(uri, '__resource%.lua$') then
@@ -48,6 +61,11 @@ function OnSetText(uri, text)
 
      if text:find("%+=") or text:find("%-=") or text:find("%/=") then
           return ''
+     end
+
+
+     if #diffs == 0 then
+             return nil
      end
 
      return diffs
